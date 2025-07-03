d3b7ba8c968af2d8fa4de3dd5d5654ed200ea5ac
flarxx <flarx3@gmail.com>
2025-07-03 11:15:47 -0400

    por ahora solo tengo creado la  estructura y un poco de estilo

---
+++ script.js
@@ -0,0 +1,5 @@
+const Hamburger = document.querySelector('.menu-toggle');
+const Sidebar = document.querySelector('.sidebar');
+const Main = document.querySelector('main');
+const Header = document.querySelector('header');
+
