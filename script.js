31920b0dc9e3c6373a9184548d6b55324f955d81
flarxx <flarx3@gmail.com>
2025-07-03 14:25:40 -0400

    agregada logica para el ciere del navbar  hay q corregir el main conten

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const Hamburger = document.querySelector('.menu-toggle');
-const Sidebar = document.querySelector('.sidebar');
-const Main = document.querySelector('main');
-const Header = document.querySelector('header');
+const navbar = document.getElementById('sidebar');
+const toggleButton = document.querySelector('.sidebar-toggle');
+const links = document.querySelectorAll('.sidebar a');
 
+toggleButton.addEventListener('click', () => {
+  navbar.classList.toggle('close');
+  toggleButton.classList.toggle('rotate'); // si quieres animar el botón
+  links.forEach(link => {
+    link.classList.toggle('active'); // opcional, si usas estilos para links
+  });
+  document.body.classList.toggle('active');
+
+  if (navbar.classList.contains('close')) {
+    toggleButton.innerHTML = '<i class="fa-solid fa-right-long"></i>';
+  } else {
+    toggleButton.innerHTML = '<i class="fa-solid fa-backward"></i>';
+  }
+});
+// Añadir un evento de clic a los enlaces del sidebar
