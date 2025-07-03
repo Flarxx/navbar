e0a3b88881ec574b943b1b864c6893506a82738d
flarxx <flarx3@gmail.com>
2025-07-03 14:58:08 -0400

    aqui ya lista la animacion para el header vertical

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const toggleButton = document.querySelector('.sidebar-toggle');
 const links = document.querySelectorAll('.sidebar a');
 
 toggleButton.addEventListener('click', () => {
+    // Alternar la clase 'close' en el sidebar para mostrar/ocultar
   navbar.classList.toggle('close');
-  toggleButton.classList.toggle('rotate'); // si quieres animar el botón
-  links.forEach(link => {
-    link.classList.toggle('active'); // opcional, si usas estilos para links
-  });
+    // Alternar la clase 'rotate' en el botón para animar el icono
+  toggleButton.classList.toggle('rotate'); 
+  
+    // si quieres animar el botón
+    // Alternar la clase 'active' en el body para aplicar estilos globales
+
   document.body.classList.toggle('active');
 
   if (navbar.classList.contains('close')) {
