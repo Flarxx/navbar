c29bff77d563590dc273bcd44ca2b49541d3b000
flarxx <flarx3@gmail.com>
2025-07-04 15:06:17 -0400

    aca ya le di estilo para mi version mobile toca corregir cuando esta cerrrado

--- script.js
+++ script.js
@@ -5,14 +5,7 @@ const links = document.querySelectorAll('.sidebar a');
 toggleButton.addEventListener('click', () => {
     // Alternar la clase 'close' en el sidebar para mostrar/ocultar
   navbar.classList.toggle('close');
-    // Alternar la clase 'rotate' en el botón para animar el icono
-  toggleButton.classList.toggle('rotate'); 
-  
-    // si quieres animar el botón
-    // Alternar la clase 'active' en el body para aplicar estilos globales
-
   document.body.classList.toggle('active');
-
   if (navbar.classList.contains('close')) {
     toggleButton.innerHTML = '<i class="fa-solid fa-right-long"></i>';
   } else {
@@ -20,3 +13,4 @@ toggleButton.addEventListener('click', () => {
   }
 });
 // Añadir un evento de clic a los enlaces del sidebar
+
